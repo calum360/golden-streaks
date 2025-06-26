42b2442a7ecb298d5012667706d2da49437ee91b
calum360 <calumlefeuvre@live.ie>
2025-06-26 23:05:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -190,24 +190,11 @@ function scrollToCard(index) {
   card.classList.add('activeCard');
 
   // Scroll to it
-/*  viewer.scrollTo({
-    top: top,
-    behavior: 'smooth'
-  });				26/06/2025 */
-    // iOS Safari fix - temporarily enable scrolling
-  viewer.style.overflowY = 'scroll';
-  viewer.style.scrollbarWidth = 'none';
-  
-  // Scroll to it
   viewer.scrollTo({
     top: top,
     behavior: 'smooth'
-  });
-  
-  // Hide scrollbar again after scroll
-  setTimeout(() => {
-    viewer.style.overflowY = 'auto';
-  }, 300); /* end */
+  });				
+   
 
   
 
