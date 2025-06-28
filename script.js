f91b7ea1f28e9e246cb7444f2b3b62cbcf2a6382
calum360 <calumlefeuvre@live.ie>
2025-06-28 10:35:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let resizeTimeout;
 
 
 const openBtn = document.getElementById('openViewerBtn');
-console.log("openBtn on mobile?", openBtn);
+
 
 
 const originalModalContent = document.querySelector('#contactModal .modal-content').innerHTML;
